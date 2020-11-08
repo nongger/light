e947c082e61a06838bea7d3ec7c2fff533eb6342
nongger <darren220@163.com>
2020-11-08 21:56:25 +0800

    【数据结构】【新增】栈

--- light-java-base/src/main/java/com/darren/AlgorithmAndDataStructures/DataStructures/ArrayStack.java
+++ light-java-base/src/main/java/com/darren/AlgorithmAndDataStructures/DataStructures/ArrayStack.java
@@ -27,6 +27,12 @@ public class ArrayStack {
         return top == -1;
     }
 
+    /**
+     * 入栈
+     *
+     * @param element
+     * @return
+     */
     public boolean push(int element) {
         // 如果超过容量返回false
         if (isFull()) {
@@ -37,11 +43,28 @@ public class ArrayStack {
         return true;
     }
 
+    /**
+     * 出栈
+     *
+     * @return
+     */
     public int pop() {
         if (isEmpty()) {
             throw new RuntimeException("栈内元素为空！");
         }
         return stack[top--];
+    }
+
+    /**
+     * 查看栈顶元素，不弹出
+     *
+     * @return
+     */
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("栈内元素为空！");
+        }
+        return stack[top];
     }
 
     public void showAll() {
@@ -52,6 +75,40 @@ public class ArrayStack {
             System.out.printf("当前栈元素：%d, 值：%d\n", i, stack[i]);
         }
     }
+
+    /**
+     * 判断运算符的优先级
+     * 此处假定只有四种简单运算
+     * 数字越大优先级越高
+     *
+     * @param oper
+     * @return
+     */
+    public int priority(int oper) {
+        if (oper == '*' || oper == '/') {
+            return 1;
+        } else if (oper == '+' || oper == '-') {
+            return 0;
+        } else {
+            return -1;
+        }
+
+    }
+
+    /**
+     * 是否为操作符
+     *
+     * @param index
+     * @return
+     */
+    public boolean isOper(char index) {
+        return index == '+' || index == '-' || index == '*' || index == '/';
+    }
+
+    public int calculate(String expression) {
+        return 0;
+    }
+
 
     public static void main(String[] args) {
         ArrayStack arrayStack = new ArrayStack(10);
