48f73b04d02a5c99e0ac52cd79ef85c4c9d80c3f
nongger <darren220@163.com>
2021-01-13 11:27:10 +0800

    banner玩法测试

---
+++ light-java-base/src/main/java/com/darren/spring/SpringInitalWay.java
@@ -0,0 +1,21 @@
+package com.darren.spring;
+
+/**
+ * Project: light
+ * Time   : 2021-01-13 10:26
+ * Desc   : Java 类的初始化顺序依次是静态变量 > 静态代码块 > 全局变量 > 初始化代码块 > 构造器
+ * <p>
+ * 如果有一些初始化操作需要在 Spring 启动的时候必须完成，如创建定时任务，创建连接池等。
+ * 本文将介绍以下几种 Spring 启动监听方式：
+ * Bean 构造函数方式
+ * 使用 @PostConstruct 注解
+ * 实现 InitializingBean 接口
+ * 监听 ApplicationListener 事件
+ * 使用 Constructor 注入方式
+ * 实现 SpringBoot 的 CommandLineRunner 接口
+ * SmartLifecycle 机制
+ */
+public class SpringInitalWay {
+
+    // 在 Spring 中将先初始化 Bean，也就是会先调用类的构造函数，然后才注入成员变量依赖的 Bean（@Autowired和@Resource注解修饰的成员变量），注意@Value等注解的配置的注入也是在构造函数之后。
+}
