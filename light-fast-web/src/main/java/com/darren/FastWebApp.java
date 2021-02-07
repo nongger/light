9dbb47ffc6817cd60f0183a14ba8ad0e1803fac5
nongger <darren220@163.com>
2021-02-07 20:45:52 +0800

    web测试项目

---
+++ light-fast-web/src/main/java/com/darren/FastWebApp.java
@@ -0,0 +1,30 @@
+package com.darren;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@Slf4j
+@SpringBootApplication
+public class FastWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FastWebApp.class, args);
+        log.info("-------------------------------------------------------------------------------");
+        log.info("|                                      |                                      |");
+        log.info("|                                --====|====--                                |");
+        log.info("|                                      |                                      |");
+        log.info("|                                                                             |");
+        log.info("|                                  .-'''''-.                                  |");
+        log.info("|                                .'_________'.                                |");
+        log.info("|                               /_/_|__|__|_\\_\\                               |");
+        log.info("|                              ;'-._       _.-';                              |");
+        log.info("|         ,--------------------|    `-. .-'    |--------------------,         |");
+        log.info("|          ``''--..__    ___   ;       '       ;   ___    __..--''``          |");
+        log.info("|                    `'-// \\\\.._\\             /_..// \\\\- '`                   |");
+        log.info("|                       \\\\_//    '._       _.'    \\\\_//                       |");
+        log.info("-------------------------------------------------------------------------------");
+    }
+}
